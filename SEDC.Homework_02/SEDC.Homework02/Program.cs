28cad8348c89d02cca1817cf795c72eabedf89a5
Antonio <novoselskiantonio@gmail.com>
2023-03-03 15:44:43 +0100

    neww

---
+++ SEDC.Homework_02/SEDC.Homework02/Program.cs
@@ -0,0 +1,31 @@
+﻿#region Exercise 01
+
+Console.WriteLine("Enter the first number: ");
+double firstUserNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+double secondUserNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the operation: ");
+char userOperation = char.Parse(Console.ReadLine());
+
+switch (userOperation)
+{
+    case '+':
+        Console.WriteLine("The result is: " + (firstUserNumber + secondUserNumber));
+        break;
+    case '-':
+        Console.WriteLine("The result is: " + (firstUserNumber - secondUserNumber));
+        break;
+    case '*':
+        Console.WriteLine("The result is: " + (firstUserNumber * secondUserNumber));
+        break;
+    case '/':
+        Console.WriteLine("The result is: " + (firstUserNumber / secondUserNumber));
+        break;
+
+
+    default:
+        Console.WriteLine("Wrong operation! Please enter the correct one");
+        break;
+}
+#endregion
