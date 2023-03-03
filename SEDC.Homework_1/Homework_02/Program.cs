23e15f8481e6dd0a84efcc0bf8f0ebdc23291c47
Antonio <novoselskiantonio@gmail.com>
2023-03-03 15:21:46 +0100

    new

---
+++ SEDC.Homework_1/Homework_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("    *");
+Console.WriteLine("    *");
+Console.WriteLine("  * * *");
+Console.WriteLine(" *  *  *");
+Console.WriteLine("    *");
+Console.WriteLine("    *");
+Console.WriteLine("  *   *");
+Console.WriteLine(" *     *");
+                              
+
---
+++ SEDC.Homework_2/Exercise_03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("After Swapping:");
+firstNumber = firstNumber * secondNumber;
+secondNumber = firstNumber / secondNumber;
+firstNumber = firstNumber / secondNumber;
+
+Console.WriteLine("First number: " + firstNumber);
+Console.WriteLine("Second number: " + secondNumber);
